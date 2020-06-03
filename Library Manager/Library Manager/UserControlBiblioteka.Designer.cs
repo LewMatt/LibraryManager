2e8b0cac92c8c9457aa66f956e1fa854c8240482
LewMatt <lewmateush@gmail.com>
2020-06-03 22:30:22 +0200

    Files updated.

--- Library Manager/Library Manager/UserControlBiblioteka.Designer.cs
+++ Library Manager/Library Manager/UserControlBiblioteka.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.listViewBiblioteka = new System.Windows.Forms.ListView();
+            this.hID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hTytul = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hAutor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hGatunek = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hDataWyd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hIlosc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // label1
@@ -40,10 +47,58 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Biblioteka";
             // 
+            // listViewBiblioteka
+            // 
+            this.listViewBiblioteka.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.hID,
+            this.hTytul,
+            this.hAutor,
+            this.hGatunek,
+            this.hDataWyd,
+            this.hIlosc});
+            this.listViewBiblioteka.HideSelection = false;
+            this.listViewBiblioteka.Location = new System.Drawing.Point(82, 148);
+            this.listViewBiblioteka.Name = "listViewBiblioteka";
+            this.listViewBiblioteka.Size = new System.Drawing.Size(600, 350);
+            this.listViewBiblioteka.TabIndex = 1;
+            this.listViewBiblioteka.UseCompatibleStateImageBehavior = false;
+            this.listViewBiblioteka.View = System.Windows.Forms.View.Details;
+            // 
+            // hID
+            // 
+            this.hID.Text = "ID";
+            this.hID.Width = 30;
+            // 
+            // hTytul
+            // 
+            this.hTytul.Text = "Tytuł";
+            this.hTytul.Width = 180;
+            // 
+            // hAutor
+            // 
+            this.hAutor.Text = "Autor";
+            this.hAutor.Width = 130;
+            // 
+            // hGatunek
+            // 
+            this.hGatunek.Text = "Gatunek";
+            this.hGatunek.Width = 80;
+            // 
+            // hDataWyd
+            // 
+            this.hDataWyd.Text = "Data wydania";
+            this.hDataWyd.Width = 80;
+            // 
+            // hIlosc
+            // 
+            this.hIlosc.Text = "Ilość dostępnych";
+            this.hIlosc.Width = 100;
+            // 
             // UserControlBiblioteka
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listViewBiblioteka);
             this.Controls.Add(this.label1);
             this.Name = "UserControlBiblioteka";
             this.Size = new System.Drawing.Size(900, 600);
@@ -55,5 +110,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewBiblioteka;
+        private System.Windows.Forms.ColumnHeader hID;
+        private System.Windows.Forms.ColumnHeader hTytul;
+        private System.Windows.Forms.ColumnHeader hAutor;
+        private System.Windows.Forms.ColumnHeader hGatunek;
+        private System.Windows.Forms.ColumnHeader hDataWyd;
+        private System.Windows.Forms.ColumnHeader hIlosc;
     }
 }
