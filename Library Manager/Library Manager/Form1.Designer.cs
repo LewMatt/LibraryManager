c347e90b0c7e522661c34e92aa51923ad76a6e64
LewMatt <lewmateush@gmail.com>
2020-06-07 23:35:48 +0200

    Files updated.

--- Library Manager/Library Manager/Form1.Designer.cs
+++ Library Manager/Library Manager/Form1.Designer.cs
@@ -35,31 +35,34 @@
             this.label2 = new System.Windows.Forms.Label();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnUtworzKonto = new System.Windows.Forms.Button();
+            this.lblLibraryManager = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxLogin
             // 
-            this.textBoxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxLogin.Location = new System.Drawing.Point(458, 181);
+            this.textBoxLogin.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxLogin.Location = new System.Drawing.Point(371, 239);
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(216, 29);
+            this.textBoxLogin.Size = new System.Drawing.Size(358, 45);
             this.textBoxLogin.TabIndex = 0;
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPassword.Location = new System.Drawing.Point(458, 345);
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPassword.Location = new System.Drawing.Point(371, 341);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
-            this.textBoxPassword.Size = new System.Drawing.Size(216, 31);
+            this.textBoxPassword.Size = new System.Drawing.Size(358, 45);
             this.textBoxPassword.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(491, 483);
+            this.btnLogin.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLogin.Location = new System.Drawing.Point(425, 480);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(130, 48);
+            this.btnLogin.Size = new System.Drawing.Size(250, 50);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Zaloguj";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -68,26 +71,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(373, 181);
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(263, 239);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(102, 47);
             this.label1.TabIndex = 3;
-            this.label1.Text = "login";
+            this.label1.Text = "Login:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(370, 345);
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(274, 344);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(91, 42);
             this.label2.TabIndex = 4;
-            this.label2.Text = "haslo";
+            this.label2.Text = "Hasło:";
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(986, 631);
+            this.btnExit.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnExit.Location = new System.Drawing.Point(1000, 0);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(81, 43);
+            this.btnExit.Size = new System.Drawing.Size(100, 50);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "Wyjście";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -95,21 +101,43 @@
             // 
             // btnUtworzKonto
             // 
-            this.btnUtworzKonto.Location = new System.Drawing.Point(900, 280);
+            this.btnUtworzKonto.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUtworzKonto.Location = new System.Drawing.Point(0, 0);
             this.btnUtworzKonto.Name = "btnUtworzKonto";
-            this.btnUtworzKonto.Size = new System.Drawing.Size(100, 50);
+            this.btnUtworzKonto.Size = new System.Drawing.Size(200, 50);
             this.btnUtworzKonto.TabIndex = 6;
             this.btnUtworzKonto.Text = "Utwórz konto";
             this.btnUtworzKonto.UseVisualStyleBackColor = true;
             this.btnUtworzKonto.Click += new System.EventHandler(this.btnUtworzKonto_Click);
+            // 
+            // lblLibraryManager
+            // 
+            this.lblLibraryManager.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblLibraryManager.Font = new System.Drawing.Font("Segoe Script", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblLibraryManager.Location = new System.Drawing.Point(0, 0);
+            this.lblLibraryManager.Name = "lblLibraryManager";
+            this.lblLibraryManager.Size = new System.Drawing.Size(1100, 150);
+            this.lblLibraryManager.TabIndex = 7;
+            this.lblLibraryManager.Text = "Library Manager ";
+            this.lblLibraryManager.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.btnUtworzKonto);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 650);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1100, 50);
+            this.panel1.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1100, 700);
-            this.Controls.Add(this.btnUtworzKonto);
-            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblLibraryManager);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnLogin);
@@ -119,6 +147,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,6 +162,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnUtworzKonto;
+        private System.Windows.Forms.Label lblLibraryManager;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Library Manager/Library Manager/FormCreateAccount.Designer.cs
+++ Library Manager/Library Manager/FormCreateAccount.Designer.cs
@@ -44,141 +44,144 @@
             this.textBoxTelefon = new System.Windows.Forms.TextBox();
             this.btnUtworzKonto = new System.Windows.Forms.Button();
             this.btnPowrot = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblLogin
             // 
             this.lblLogin.AutoSize = true;
-            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblLogin.Location = new System.Drawing.Point(321, 81);
+            this.lblLogin.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblLogin.Location = new System.Drawing.Point(266, 80);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(71, 25);
+            this.lblLogin.Size = new System.Drawing.Size(78, 36);
             this.lblLogin.TabIndex = 0;
             this.lblLogin.Text = "Login:";
             // 
             // lblHaslo
             // 
             this.lblHaslo.AutoSize = true;
-            this.lblHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblHaslo.Location = new System.Drawing.Point(319, 150);
+            this.lblHaslo.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblHaslo.Location = new System.Drawing.Point(265, 147);
             this.lblHaslo.Name = "lblHaslo";
-            this.lblHaslo.Size = new System.Drawing.Size(73, 25);
+            this.lblHaslo.Size = new System.Drawing.Size(79, 36);
             this.lblHaslo.TabIndex = 1;
             this.lblHaslo.Text = "Hasło:";
             // 
             // lblPowHaslo
             // 
             this.lblPowHaslo.AutoSize = true;
-            this.lblPowHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblPowHaslo.Location = new System.Drawing.Point(239, 208);
+            this.lblPowHaslo.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPowHaslo.Location = new System.Drawing.Point(171, 220);
             this.lblPowHaslo.Name = "lblPowHaslo";
-            this.lblPowHaslo.Size = new System.Drawing.Size(153, 25);
+            this.lblPowHaslo.Size = new System.Drawing.Size(173, 36);
             this.lblPowHaslo.TabIndex = 2;
             this.lblPowHaslo.Text = "Powtórz hasło:";
             // 
             // lblImie
             // 
             this.lblImie.AutoSize = true;
-            this.lblImie.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblImie.Location = new System.Drawing.Point(335, 270);
+            this.lblImie.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblImie.Location = new System.Drawing.Point(277, 290);
             this.lblImie.Name = "lblImie";
-            this.lblImie.Size = new System.Drawing.Size(57, 25);
+            this.lblImie.Size = new System.Drawing.Size(67, 36);
             this.lblImie.TabIndex = 3;
             this.lblImie.Text = "Imię:";
             // 
             // lblNazwisko
             // 
             this.lblNazwisko.AutoSize = true;
-            this.lblNazwisko.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblNazwisko.Location = new System.Drawing.Point(282, 330);
+            this.lblNazwisko.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblNazwisko.Location = new System.Drawing.Point(225, 360);
             this.lblNazwisko.Name = "lblNazwisko";
-            this.lblNazwisko.Size = new System.Drawing.Size(110, 25);
+            this.lblNazwisko.Size = new System.Drawing.Size(119, 36);
             this.lblNazwisko.TabIndex = 4;
             this.lblNazwisko.Text = "Nazwisko:";
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblEmail.Location = new System.Drawing.Point(321, 392);
+            this.lblEmail.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblEmail.Location = new System.Drawing.Point(263, 430);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(71, 25);
+            this.lblEmail.Size = new System.Drawing.Size(81, 36);
             this.lblEmail.TabIndex = 5;
             this.lblEmail.Text = "Email:";
             // 
             // lblTelefon
             // 
             this.lblTelefon.AutoSize = true;
-            this.lblTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblTelefon.Location = new System.Drawing.Point(228, 453);
+            this.lblTelefon.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTelefon.Location = new System.Drawing.Point(156, 500);
             this.lblTelefon.Name = "lblTelefon";
-            this.lblTelefon.Size = new System.Drawing.Size(164, 25);
+            this.lblTelefon.Size = new System.Drawing.Size(188, 36);
             this.lblTelefon.TabIndex = 6;
             this.lblTelefon.Text = "Numer telefonu:";
             // 
             // textBoxLogin
             // 
-            this.textBoxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxLogin.Location = new System.Drawing.Point(415, 78);
+            this.textBoxLogin.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxLogin.Location = new System.Drawing.Point(350, 80);
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(300, 31);
+            this.textBoxLogin.Size = new System.Drawing.Size(400, 36);
             this.textBoxLogin.TabIndex = 7;
             // 
             // textBoxHaslo
             // 
-            this.textBoxHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxHaslo.Location = new System.Drawing.Point(415, 147);
+            this.textBoxHaslo.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxHaslo.Location = new System.Drawing.Point(350, 150);
             this.textBoxHaslo.Name = "textBoxHaslo";
             this.textBoxHaslo.PasswordChar = '*';
-            this.textBoxHaslo.Size = new System.Drawing.Size(300, 31);
+            this.textBoxHaslo.Size = new System.Drawing.Size(400, 33);
             this.textBoxHaslo.TabIndex = 8;
             // 
             // textBoxPowHaslo
             // 
-            this.textBoxPowHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxPowHaslo.Location = new System.Drawing.Point(415, 205);
+            this.textBoxPowHaslo.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPowHaslo.Location = new System.Drawing.Point(350, 220);
             this.textBoxPowHaslo.Name = "textBoxPowHaslo";
             this.textBoxPowHaslo.PasswordChar = '*';
-            this.textBoxPowHaslo.Size = new System.Drawing.Size(300, 31);
+            this.textBoxPowHaslo.Size = new System.Drawing.Size(400, 36);
             this.textBoxPowHaslo.TabIndex = 9;
             // 
             // textBoxImie
             // 
-            this.textBoxImie.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxImie.Location = new System.Drawing.Point(415, 267);
+            this.textBoxImie.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxImie.Location = new System.Drawing.Point(350, 290);
             this.textBoxImie.Name = "textBoxImie";
-            this.textBoxImie.Size = new System.Drawing.Size(300, 31);
+            this.textBoxImie.Size = new System.Drawing.Size(400, 36);
             this.textBoxImie.TabIndex = 10;
             // 
             // textBoxNazwisko
             // 
-            this.textBoxNazwisko.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxNazwisko.Location = new System.Drawing.Point(415, 327);
+            this.textBoxNazwisko.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxNazwisko.Location = new System.Drawing.Point(350, 360);
             this.textBoxNazwisko.Name = "textBoxNazwisko";
-            this.textBoxNazwisko.Size = new System.Drawing.Size(300, 31);
+            this.textBoxNazwisko.Size = new System.Drawing.Size(400, 36);
             this.textBoxNazwisko.TabIndex = 11;
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxEmail.Location = new System.Drawing.Point(415, 389);
+            this.textBoxEmail.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxEmail.Location = new System.Drawing.Point(350, 430);
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(300, 31);
+            this.textBoxEmail.Size = new System.Drawing.Size(400, 36);
             this.textBoxEmail.TabIndex = 12;
             // 
             // textBoxTelefon
             // 
-            this.textBoxTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxTelefon.Location = new System.Drawing.Point(415, 450);
+            this.textBoxTelefon.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxTelefon.Location = new System.Drawing.Point(350, 500);
             this.textBoxTelefon.Name = "textBoxTelefon";
-            this.textBoxTelefon.Size = new System.Drawing.Size(300, 31);
+            this.textBoxTelefon.Size = new System.Drawing.Size(400, 36);
             this.textBoxTelefon.TabIndex = 13;
             // 
             // btnUtworzKonto
             // 
-            this.btnUtworzKonto.Location = new System.Drawing.Point(454, 563);
+            this.btnUtworzKonto.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUtworzKonto.Location = new System.Drawing.Point(425, 580);
             this.btnUtworzKonto.Name = "btnUtworzKonto";
-            this.btnUtworzKonto.Size = new System.Drawing.Size(200, 50);
+            this.btnUtworzKonto.Size = new System.Drawing.Size(250, 50);
             this.btnUtworzKonto.TabIndex = 14;
             this.btnUtworzKonto.Text = "Utwórz konto";
             this.btnUtworzKonto.UseVisualStyleBackColor = true;
@@ -186,7 +189,8 @@
             // 
             // btnPowrot
             // 
-            this.btnPowrot.Location = new System.Drawing.Point(988, 638);
+            this.btnPowrot.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPowrot.Location = new System.Drawing.Point(1000, 0);
             this.btnPowrot.Name = "btnPowrot";
             this.btnPowrot.Size = new System.Drawing.Size(100, 50);
             this.btnPowrot.TabIndex = 15;
@@ -194,12 +198,21 @@
             this.btnPowrot.UseVisualStyleBackColor = true;
             this.btnPowrot.Click += new System.EventHandler(this.btnPowrot_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnPowrot);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 650);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1100, 50);
+            this.panel1.TabIndex = 16;
+            // 
             // FormCreateAccount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1100, 700);
-            this.Controls.Add(this.btnPowrot);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnUtworzKonto);
             this.Controls.Add(this.textBoxTelefon);
             this.Controls.Add(this.textBoxEmail);
@@ -219,6 +232,7 @@
             this.Name = "FormCreateAccount";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormCreateAccount";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -242,5 +256,6 @@
         private System.Windows.Forms.TextBox textBoxTelefon;
         private System.Windows.Forms.Button btnUtworzKonto;
         private System.Windows.Forms.Button btnPowrot;
+        private System.Windows.Forms.Panel panel1;
     }
 }
